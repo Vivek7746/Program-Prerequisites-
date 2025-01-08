3130c01d3e84ea6a08a34f6cae629c0e23076c0b
Vivek <vivekthegamer999@gmail.com>
2025-01-08 11:16:58 +0530

    Volume of a Cylinder

---
+++ Problem5.java
@@ -0,0 +1,12 @@
+import java.util.*;
+class Problem5{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int Radius = sc.nextInt();
+		int Height = sc.nextInd();
+		int Volume = (22/7) * Radius * Radius * Height; 
+		
+		System.out.print(Area);
+	}
+}
