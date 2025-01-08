56e752ba5b103c9991e87779f0721ee8f2a96e09
Vivek <vivekthegamer999@gmail.com>
2025-01-08 11:10:39 +0530

    Area problem

---
+++ Problem4.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Problem4{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int Radius = sc.nextInt();
+		int Area = (22/7) * Radius * Radius; 
+		
+		System.out.print(Area);
+	}
+}
