2279023d1fe8e0bf2572562ea33b81a8614e04c5
Vivek <vivekthegamer999@gmail.com>
2025-01-08 11:46:53 +0530

    Average of three numbers

---
+++ Problem9.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Problem9{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int Num1 = sc.nextInt();
+		int Num2 = sc.nextInt();
+		int Num3 = sc.nextInt();
+		
+		double Average = (Num1 + Num2 + Num3)/3;
+		
+		System.out.print(Average);
+	}
+}
