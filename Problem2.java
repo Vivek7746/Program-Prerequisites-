1eee3aa29f9109c89c6dc2fb3b931052a41679cd
Vivek <vivekthegamer999@gmail.com>
2025-01-08 10:24:46 +0530

    Sum code

---
+++ Problem2.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Problem2{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.Print(a+b);
+	}
+}
