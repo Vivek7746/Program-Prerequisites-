dc1c550bb842747e8ee87bc934cb9c46abbfe314
Vivek <vivekthegamer999@gmail.com>
2025-01-08 10:50:23 +0530

    Conversion problem

---
+++ Problem3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Problem3{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		int Celsius = sc.nextInt();
+		int b = sc.nextInt();
+		
+		System.out.Print(a+b);
+	}
+}
